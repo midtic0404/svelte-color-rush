1d6ffbda78645f62d2ce22cde4e1fc003c3fbde9
cchechang <cchechang@mastercontrol.com>
2020-07-07 21:25:40 -0600

    Svelte color rush game

---
+++ src/App.svelte
@@ -0,0 +1,193 @@
+<script>
+	import Circle from "./Circle.svelte";
+
+	let gameRows = [];
+	let isPlaying = false;
+	let score = 0;
+	let timeLeft = 0;
+	let gameInterval = null;
+
+	function startGame() {
+		score = 0;
+		isPlaying = true;
+		startTimer();
+		setUpRows();
+	}
+
+	function startTimer(){
+		timeLeft = 5;
+		gameInterval = setInterval(() => {
+
+			if(timeLeft > 1){
+				timeLeft = timeLeft - 1;
+			}else {
+				gameOver();
+			}
+		}, 1000);
+	};
+
+	function gameOver() {
+		isPlaying = false;
+		stopTimer();
+	};
+
+	function circleClicked(isTarget) {
+		if (isTarget) {
+			score = score + 1;
+			resetTimer();
+			setUpRows();
+		} else {
+			timeLeft = timeLeft - 1;
+			if(timeLeft <= 0){
+				gameOver();
+			}
+		}
+  	};
+
+	function resetTimer() {
+		stopTimer();
+		startTimer();
+	};
+
+	function stopTimer(){
+		clearInterval(gameInterval);
+		gameInterval = null;
+	};
+
+	function setUpRows() {
+		let baseColor = Math.floor(Math.random() * 256);
+		let restColors = Math.floor(Math.random() * 96) + 80;
+		let red = restColors;
+		let green = restColors;
+		let blue = restColors;
+		let offset = Math.floor(Math.random() * 2) === 0 ? -20 : 20;
+		let randRGB = Math.floor(Math.random() * 3);
+		let normalColor = '';
+		let targetColor = '';
+
+		if (randRGB === 0) {
+		red = baseColor;
+		normalColor = `rgb(${red}, ${blue}, ${green})`;
+		blue += offset;
+		green += offset;
+		} else if (randRGB === 1) {
+		green = baseColor;
+		normalColor = `rgb(${red}, ${blue}, ${green})`;
+		red += offset;
+		blue += offset;
+		} else {
+		blue = baseColor;
+		normalColor = `rgb(${red}, ${blue}, ${green})`;
+		red += offset;
+		green += offset;
+    }
+
+    targetColor = `rgb(${red}, ${blue}, ${green})`;
+
+    let rows = [];
+    let newTargetRow = Math.floor(Math.random() * 3);
+	let newTargetColumn = Math.floor(Math.random() * 3);
+	
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        let color = normalColor;
+        let isTarget = false;
+        if (i === newTargetRow && j === newTargetColumn) {
+          color = targetColor;
+          isTarget = true;
+        }
+        row.push(
+			{
+				key: `${i}, ${j}`,
+				color: color,
+				isTarget: isTarget
+			}
+        );
+      }
+      rows.push(row);
+	}
+	gameRows = rows;
+  };
+
+</script>
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Pangolin&display=swap');
+
+	* {
+		box-sizing: border-box;
+		font-family: 'Pangolin', cursive;
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+	}
+
+	.count-down {
+		font-size: 3rem;
+	}
+
+	.board {
+		width: 400px;
+		margin-bottom: 2rem;
+	}
+
+	.row {
+		display: flex;
+		width: 100%;
+		justify-content: space-evenly;
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
+
+	.score{
+		font-size: 60px;
+		margin: 0;
+	}
+
+	.button {
+		outline: none;
+		border: 3px solid black;
+		background-color: white;
+		font-size: 30px;
+		border-radius: 20px;
+		padding: 20px 40px;
+		cursor: pointer;
+	}
+	
+</style>
+
+
+<main>
+	{#if isPlaying}
+		<h1 class='count-down'>{timeLeft}</h1>
+		<div class="board">
+			{#each gameRows as circleRow}
+				<div class="row">
+					{#each circleRow as circleInfo}
+						<Circle 
+						color={circleInfo.color} 
+						isTarget={circleInfo.isTarget}
+						circleClicked={circleClicked}/>
+					{/each}
+				</div>
+			{/each}
+		</div>
+	{/if}
+	<h1 class='score'>Score: {score}</h1>
+	{#if !isPlaying}
+		<button class='button' on:click={startGame}>
+        	Start Game
+        </button>
+	{/if}
+</main>
+
+
---
+++ src/Circle.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let color;
+    export let isTarget;
+    export let circleClicked;
+    
+</script>
+
+<style>
+    .circle {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border: black solid 3px;
+        cursor: pointer;
+        transition: 0.1s ease;
+    }
+</style>
+
+<div
+      class='circle'
+      style="background-color: {color}"
+      on:click={circleClicked(isTarget)}
+    ></div>
+
